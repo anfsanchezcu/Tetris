1245a22ffc520be295fd10ccbbfabc2a17d72b31
Andres Sanchez <anfsanchezcu@unal.edu.co>
2021-08-24 15:36:15 -0500

    deteccion de colisiones

--- js/juego.js
+++ js/juego.js
@@ -32,8 +32,8 @@ var tablero = [
     [1,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,1,1,1,1,0,0,1],
+    [1,0,0,0,0,1,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1]
 ]
@@ -46,36 +46,36 @@ var fichaGrafico=[
     ],
     [
         [0,0,0,0],
-        [0,1,1,0],
-        [0,1,0,0],
-        [0,1,0,0]
+        [0,2,2,0],
+        [0,2,0,0],
+        [0,2,0,0]
     ],
     [
         [0,0,0,0],
-        [0,1,0,0],
-        [0,1,1,0],
-        [0,1,0,0]
+        [0,3,0,0],
+        [0,3,3,0],
+        [0,3,0,0]
     ],
     [
-        [0,1,0,0],
-        [0,1,0,0],
-        [0,1,0,0],
-        [0,1,0,0]
+        [0,4,0,0],
+        [0,4,0,0],
+        [0,4,0,0],
+        [0,4,0,0]
     ],
     [
         [0,0,0,0],
-        [0,1,0,0],
-        [0,1,0,0],
-        [0,1,1,0]
+        [0,5,0,0],
+        [0,5,0,0],
+        [0,5,5,0]
     ],[
         [0,0,0,0],
-        [1,1,0,0],
-        [0,1,1,0],
+        [6,6,0,0],
+        [0,6,6,0],
         [0,0,0,0]
     ],[
         [0,0,0,0],
-        [0,0,1,1],
-        [0,1,1,0],
+        [0,0,7,7],
+        [0,7,7,0],
         [0,0,0,0]
     ]
 ]
@@ -95,34 +95,65 @@ var objPieza = function(){
     this.x =1;
     this.y =7;
 
-    this.tipo = Math.floor(Math.random()*7);
-    this.pieza = fichaGrafico[this.tipo];
+    this.tipo ;
+    this.pieza ;
     this.aux;
     this.velocidad=0;
     
     this.nueva =function(){
         this.tipo = Math.floor(Math.random()*7);
+        console.log(this.tipo)
+        this.pieza = fichaGrafico[this.tipo];
         this.x=4;
         this.y=0;
     }
     this.caer=function(){ 
-        if(this.velocidad< 30){
+        if(this.velocidad< 30 ){
             this.velocidad++
-        }else{
+        }else if(this.colision(this.y+1,this.x,this.pieza)==false){
             this.y++;
             this.velocidad=0;
+        }else{
+            this.fijar();
+            this.nueva();
         }
+    }
+    this.colision = function(yNueva, xNueva,anguloN){
+        var colision = false;
+        var aux = anguloN;
+        for(var i=0; i<4; i++){
+            for(var j=0; j<4; j++){
+                 if(aux[i][j]!=0 && tablero[yNueva +i][xNueva+j]!=0)
+                    colision =true;
+            }
+        }
+        return colision;
     }
     this.dibuja = function(){
         for(var i = 0;i<4;i++){
             for(var j=0;j<4;j++){
 
-                if(this.pieza[i][j]==1) {
-                    ctx.fillStyle=colores[this.tipo];
+                if(this.pieza[i][j]!=0) {
+                    ctx.fillStyle=colores[this.tipo]    ;
                     ctx.fillRect((this.x+j-1)*anchoF, (this.y+i-4)*altoF,anchoF,altoF);
                 }
             }
         }
+    }
+    this.fijar=function(){
+        for( var i=0; i<4;i++){
+            for(var j =0;j<4;j++){
+                if(this.pieza[i][j]!=0){
+                    tablero[this.y+i][this.x+j]=this.pieza[i][j];
+                }
+            }
+        }
+    }
+
+
+    this.arriba= function(){
+        if(this.colision(this.y,this.x,this.rotar())==false)
+            this.pieza = this.rotar();
     }
     this.rotar = function (){
         this.aux=[
@@ -139,18 +170,20 @@ var objPieza = function(){
             }
             position--;
         }
-        this.pieza =this.aux;
-        console.log(this.pieza);
+        return this.aux;
     }
     this.abajo = function(){
-        this.y++;
+        if(this.colision(this.y+1,this.x,this.pieza)==false)
+            this.y++;
     }
     this.derecha = function(){
-        this.x++;
+        if(this.colision(this.y+1,this.x+1,this.pieza)==false)
+            this.x++;
 
     }
     this.izquierda = function(){
-        this.x--;
+        if(this.colision(this.y+1,this.x-1,this.pieza)==false)
+            this.x--;
     }
 
     this.nueva();
@@ -161,7 +194,7 @@ function dibujarTablero(){
     for(var i =4 ; i<altoT;i++){
         for(var j = 1; j<anchoT;j++){
             if(tablero[i][j]!=0){
-                ctx.fillStyle = colores[tablero[i][j]];
+                ctx.fillStyle = colores[tablero[i][j]-1];
                 ctx.fillRect((j-1)*anchoF, (i-4)*altoF,anchoF,altoF); 
             }
 
@@ -173,7 +206,7 @@ function inicializaTeclado(){
         if(e.keyCode == 37){
             pieza.izquierda();
         }else if(e.keyCode==38){
-            pieza.rotar();
+            pieza.arriba();
         }else if(e.keyCode == 39){
             pieza.derecha();
         }else if(e.keyCode==40){
